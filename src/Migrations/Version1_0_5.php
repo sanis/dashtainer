c37b5001b3a1a0a24eb5c36a6bb20fdecfd88f4f
Juan Treminio <jtreminio@gmail.com>
2018-05-07 22:00:46 -0500

    Full migration of MariaDB secrets

--- src/Migrations/Version1_0_5.php
+++ src/Migrations/Version1_0_5.php
@@ -2,13 +2,7 @@
 
 namespace Dashtainer\Migrations;
 
-use Dashtainer\Domain\Docker\ServiceWorker as Worker;
-use Dashtainer\Form\Docker\Service as Form;
-use Dashtainer\Entity\Docker as Entity;
-use Dashtainer\Repository\Docker as Repository;
-
 use Doctrine\DBAL\Schema\Schema;
-use Symfony\Component\Yaml\Yaml;
 
 class Version1_0_5 extends FixtureMigrationAbstract
 {
@@ -68,19 +62,6 @@ class Version1_0_5 extends FixtureMigrationAbstract
         $this->addSql('
             DROP TABLE docker_services_secrets;
         ');
-
-        $em = $this->container->get('doctrine.orm.entity_manager');
-
-        /** @var Repository\Service $serviceRepo */
-        $serviceRepo     = $em->getRepository(Entity\Service::class);
-        /** @var Repository\Network $networkRepo */
-        $networkRepo     = $em->getRepository(Entity\Network::class);
-        /** @var Repository\Secret $secretRepo */
-        $secretRepo      = $em->getRepository(Entity\Secret::class);
-        /** @var Repository\ServiceType $serviceTypeRepo */
-        $serviceTypeRepo = $em->getRepository(Entity\ServiceType::class);
-
-        $this->migrateMariaDB($serviceRepo, $networkRepo, $secretRepo, $serviceTypeRepo);
     }
 
     public function down(Schema $schema)
@@ -89,89 +70,5 @@ class Version1_0_5 extends FixtureMigrationAbstract
 
     public function postUp(Schema $schema)
     {
-    }
-
-    protected function migrateMariaDB(
-        Repository\Service $serviceRepo,
-        Repository\Network $networkRepo,
-        Repository\Secret $secretRepo,
-        Repository\ServiceType $serviceTypeRepo
-    ) {
-        $em = $this->container->get('doctrine.orm.entity_manager');
-        $conn = $em->getConnection();
-        $stmt = $conn->query("
-            SELECT
-                ds.project_id project_id,
-                ds.id service_id,
-                ds.name service_name,
-                ds.environment
-            FROM docker_service ds
-            JOIN docker_service_type dst ON ds.service_type_id = dst.id
-            WHERE dst.slug = 'mariadb'
-              AND ds.environment IS NOT NULL
-        ");
-
-        foreach ($stmt->fetchAll() as $row) {
-            $env  = json_decode($row['environment'], true);
-            $slug = Transliterator::urlize($row['service_name']);
-
-            $conn->insert('docker_secret', [
-                'project_id' => $row['project_id'],
-                'owner_id'   => $row['service_id'],
-                'name'       => "{$slug}-mysql_host",
-                'file'       => "./secrets/{$slug}-mysql_host",
-                'contents'   => $slug,
-                'created_at' => (new \DateTime())->format(\DateTime::ATOM),
-                'updated_at' => (new \DateTime())->format(\DateTime::ATOM),
-            ]);
-
-            $conn->insert('docker_secret', [
-                'project_id' => $row['project_id'],
-                'owner_id'   => $row['service_id'],
-                'name'       => "{$slug}-mysql_root_password",
-                'file'       => "./secrets/{$slug}-mysql_root_password",
-                'contents'   => $env['MYSQL_ROOT_PASSWORD'],
-                'created_at' => (new \DateTime())->format(\DateTime::ATOM),
-                'updated_at' => (new \DateTime())->format(\DateTime::ATOM),
-            ]);
-
-            $conn->insert('docker_secret', [
-                'project_id' => $row['project_id'],
-                'owner_id'   => $row['service_id'],
-                'name'       => "{$slug}-mysql_database",
-                'file'       => "./secrets/{$slug}-mysql_database",
-                'contents'   => $env['MYSQL_DATABASE'],
-                'created_at' => (new \DateTime())->format(\DateTime::ATOM),
-                'updated_at' => (new \DateTime())->format(\DateTime::ATOM),
-            ]);
-
-            $conn->insert('docker_secret', [
-                'project_id' => $row['project_id'],
-                'owner_id'   => $row['service_id'],
-                'name'       => "{$slug}-mysql_user",
-                'file'       => "./secrets/{$slug}-mysql_user",
-                'contents'   => $env['MYSQL_USER'],
-                'created_at' => (new \DateTime())->format(\DateTime::ATOM),
-                'updated_at' => (new \DateTime())->format(\DateTime::ATOM),
-            ]);
-
-            $conn->insert('docker_secret', [
-                'project_id' => $row['project_id'],
-                'owner_id'   => $row['service_id'],
-                'name'       => "{$slug}-mysql_password",
-                'file'       => "./secrets/{$slug}-mysql_password",
-                'contents'   => $env['MYSQL_PASSWORD'],
-                'created_at' => (new \DateTime())->format(\DateTime::ATOM),
-                'updated_at' => (new \DateTime())->format(\DateTime::ATOM),
-            ]);
-        }
-
-        $mariaDb = new Worker\MariaDB($serviceRepo, $networkRepo, $secretRepo, $serviceTypeRepo);
-        $mariaDbForm = new Form\MariaDBCreate();
-        //$mariaDbForm->
-
-        $mariaDbReflectionMethod = new \ReflectionMethod(Worker\MariaDB::class, 'createSecrets');
-
-        $a = '';
     }
 }
